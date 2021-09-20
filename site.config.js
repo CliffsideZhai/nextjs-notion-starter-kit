2af9722a19bad175d0bc9c59b4c49b71ccd9a8c6
Cliffside <875174996@qq.com>
2021-09-20 14:57:45 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Blog-caee023157c7423db4a9645cd891e013',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Blog of Cl1ffs1de',
+  domain: 'https://cl1ffs1de.notion.site/cl1ffs1de/Blog-caee023157c7423db4a9645cd891e013',
+  author: 'Cl1ffs1de Zhai',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Some of us get dipped in flat, some in satin, some in gloss.... But every once in a while you find someone who is iridescent, and when you do, nothing will ever compare. I hava found her',
+  socialImageTitle: 'Cl1ffs1de Zhai',
+  socialImageSubtitle: '',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
